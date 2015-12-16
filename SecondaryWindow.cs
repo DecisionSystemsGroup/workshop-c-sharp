416297741dabfbef4bfc2114465260cbe82e21fc
Theodore Kounas <teokounas@gmail.com>
2015-12-16 16:34:10 +0200

    Smthin

---
+++ SecondaryWindow.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class SecondaryWindow : Form
+    {
+        public SecondaryWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 f1 = new Form1();
+            f1.Show();
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ThirdWindow t1 = new ThirdWindow();
+            t1.Show();
+            this.Close();
+        }
+    }
+}
